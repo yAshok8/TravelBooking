a3a3314d972a3b5b07489ea2fc30cb842e662540
Ashok <yashok42@gmail.com>
2021-10-17 12:51:10 +0530

    Added routes for existing tabs in places page

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -1,3 +1,4 @@
+import { PlaceDetailPageModule } from './discover/place-detail/place-detail.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -5,16 +6,54 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'
+          }
+        ]
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: './offers/new-offer/new-offers.module#NewOfferPageModule'
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'
+          },
+          {
+            path: ':placeId',
+            loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { PlacesPageRoutingModule } from './places-routing.module';
@@ -11,7 +9,6 @@ import { PlacesPage } from './places.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     PlacesPageRoutingModule
   ],
