c9a11a2b7b1f10ae653d0f1bad4fd0e50f3f1af3
Ashok <yashok42@gmail.com>
2021-10-21 13:40:31 +0530

    Added Auth service operates on boolean attribute only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './auth/auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onLogOut() {
     console.log('Logging Out... ');
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,11 @@ export class AuthService {
     return this._isUserAuthenticated;
   }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   login() {
     this._isUserAuthenticated = true;
+    this.router.navigateByUrl('/places/tabs/discover');
   }
 
   logout() {
